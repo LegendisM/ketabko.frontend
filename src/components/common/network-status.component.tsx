1ebfd666be68a5433138b400c0464fd8119e4f10
LegendisM <makenkap@gmail.com>
2023-07-26 09:46:42 +0330

    Add whitelist of status codes for ignore from NetworkStatus

--- src/components/common/network-status.component.tsx
+++ src/components/common/network-status.component.tsx
@@ -6,16 +6,21 @@ import { useSetState } from "react-use";
 import Loading from "./loading";
 import { i18n } from "@/i18n/i18n";
 import { IResponseError } from "@/common/interfaces/common/error.interface";
+import _ from "lodash";
 
-const NetworkStatus: FC<PropsWithChildren & { loading: boolean, error: AxiosError<IResponseError> | null, onRetry?(): void }> = ({ loading, error, onRetry, children }) => {
+const NetworkStatus: FC<PropsWithChildren & { loading: boolean, error: AxiosError<IResponseError> | null, whitelist?: number[], onRetry?(): void }> = ({ loading, error, whitelist = [200, 201, 401, 403], onRetry, children }) => {
     const [info, setInfo] = useSetState({ init: false, enabled: false, content: '', canRetry: false });
 
     useEffect(() => {
-        if (error?.response?.status == 404) {
-            setInfo({ enabled: true, content: i18n('errors:not-found-404'), canRetry: false });
-        } else if (error?.response?.status) {
-            setInfo({ enabled: true, content: i18n('errors:internal-error'), canRetry: false });
-        } else if (error?.code) {
+        const status = error?.response?.status ?? 0;
+        const code = error?.code;
+        if (code && _.includes(whitelist, status) == false) {
+            if (status == 404) {
+                setInfo({ enabled: true, content: i18n('errors:not-found-404'), canRetry: false });
+            } else {
+                setInfo({ enabled: true, content: i18n('errors:internal-error'), canRetry: true });
+            }
+        } else if (code == "ERR_NETWORK") {
             setInfo({ enabled: true, content: i18n('errors:internet-error'), canRetry: true });
         } else {
             setInfo({ enabled: false });
